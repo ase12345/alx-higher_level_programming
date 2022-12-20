b727213d61b84e9f3fd6a8bc0cdbeda6ff57fb9b
ase12345 <asefa.mulu54@gmail.com>
2022-12-20 11:50:37 +0300

    Python exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
-
-
-def safe_print_list(my_list=[], x=0):
-    i = 0
-    printed = 0
-    for i in range(0, x):
-        try:
-            print("{}".format(my_list[i]), end="")
-            printed += 1
-        except:
-            continue
-    print()
-    return printed
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+    except:
+        return False
+    else:
+        return True
