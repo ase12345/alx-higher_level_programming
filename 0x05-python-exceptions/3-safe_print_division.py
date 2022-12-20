42428dbd05fd5c3b69843a44ee23bda74659d87d
ase12345 <asefa.mulu54@gmail.com>
2022-12-20 16:16:01 +0300

    Integers division with debug

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-def safe_print_list_integers(my_list=[], x=0):
-    count = 0
-    for i in range(x):
-        try:
-            print("{:d}".format(my_list[i]), end="")
-            count += 1
-        except (ValueError, TypeError):
-            continue
-    print("")
-    return (count)
+def safe_print_division(a, b):
+    try:
+        x = a / b
+    except ZeroDivisionError:
+        x = None
+    finally:
+        print("Inside result: {}".format(x))
+        return 
