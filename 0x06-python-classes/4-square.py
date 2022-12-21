bd689cf34f83ed35a1b392ab817e4a3e1add0992
ase12345 <asefa.mulu54@gmail.com>
2022-12-21 09:44:13 +0300

    Access and update private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""Documentation for a square class"""
+
+
+class Square():
+    """Square class for quadrilateral with four equal sides"""
+
+    def __init__(self, size=0):
+        """Sets the size of the square on instantiation
+           Throws an error if the size passed in is not an integer
+        Args:
+            size (int, optional): the size of the square object
+        Raises:
+            TypeError: When the value passed in is not an integer
+            ValueError: When the value passed in is less than 0
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    @property
+    def size(self):
+        """Returns the current size of the object
+           If called with a value, the setter function overwrites the
+           current size with the size passed in
+        Returns:
+            size of the current object
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Resets the size of the object with the value called
+        Args:
+            value (int): the new size of the square object
+        Raises:
+            TypeError: When the value passed in is not an integer
+            ValueError: When the value passed in is less than 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """Returns the area of the current square object
+        Returns:
+            the area of the current square object
+        """
+        return self.__size ** 2
