089d0012a3dec615e0078cc9ad50c304ffa9204d
ase12345 <asefa.mulu54@gmail.com>
2023-01-10 21:44:32 +0300

    class to json

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+"""class_to_json module.
+Contains a function that returns a dictionary.
+"""
 
 
-import json
-
-
-def load_from_json_file(filename):
-    with open(filename, "r", encoding="UTF-8") as f:
-        return json.load(f)
+def class_to_json(obj):
+    """
+    Returns the dictionary description with simple data
+    structure (list, dictionary, string, integer and boolean)
+    for JSON serialization of an object.
+    """
+    return obj.__dict__
