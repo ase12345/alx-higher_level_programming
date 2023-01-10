b402e931526c9ab1aeb9669f2378d1e1d7d0506f
ase12345 <asefa.mulu54@gmail.com>
2023-01-10 15:20:29 +0300

    Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-"""Add item script."""
-import sys
+"""0x0B. Python - Input/Output, task 9. Load, add, save  """
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+from sys import argv
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+
+argv_edit = argv[1:]
 
 try:
-    lst = load_from_json_file("add_item.json")
+    content_list = load_from_json_file("add_item.json")
 except:
-    lst = []
-
-argc = len(sys.argv)
-
-if argc > 1:
-    for i in range(1, argc):
-        lst.append(sys.argv[i])
-
-save_to_json_file(lst, "add_item.json")
+    content_list = []
+finally:
+    for arg in argv_edit:
+        content_list.append(arg)
+    save_to_json_file(content_list, "add_item.json")
